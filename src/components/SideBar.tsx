211290dc15eb2487a076c83eb7ff1d683b451200
t6d <22022115@vnu.edu.vn>
2024-10-30 17:57:25 +0700

    fix SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -21,7 +21,7 @@ const SideBar: React.FC = () => {
     <div style={sidebarStyle} className="flex flex-col bg-main1-1 h-dvh w-[60px] items-center">
       <img src="/ICE.png" alt="logo_ice" className="h-[18px] object-contain my-8"/>
       <div className="flex flex-col w-full">
-        <Link className={`py-4 ${location.pathname ==="/programs"? "bg-main2-1" : "bg-main1-1"} bg-main2-1 w-full flex flex-col items-center`} to={"/programs"}>
+        <Link className={`py-4 ${location.pathname ==="/programs"? "bg-main2-1" : "bg-main1-1"} bg-main1-1 w-full flex flex-col items-center`} to={"/programs"}>
           <FiTarget className="stroke-white size-6" />
         </Link>
         <Link className={`py-4 ${location.pathname ==="/leaderboard"? "bg-main2-1" : "bg-main1-1"} w-full flex flex-col items-center`} to={"/leaderboard"}>
