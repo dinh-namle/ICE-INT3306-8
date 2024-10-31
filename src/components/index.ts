bf0815f064d69a9e8dd65c8f905f35a6fe433517
t6d <22022115@vnu.edu.vn>
2024-11-01 00:29:00 +0700

    change file path

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
+export * from './Admin';
 export {default as Header} from './Header';
 export {default as Footer} from './Footer';
 export {default as Sidebar} from './SideBar';
-export {default as AdminSidebar} from './AdminSideBar';
-export {default as Menu} from './Menu';
