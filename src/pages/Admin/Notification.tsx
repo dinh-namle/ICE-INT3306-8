c46f3cd8d5cf26310df57d0a1c5e329468185154
t6d <22022115@vnu.edu.vn>
2024-11-01 14:40:48 +0700

    fix notification bug

--- src/pages/Admin/Notification.tsx
+++ src/pages/Admin/Notification.tsx
@@ -11,23 +11,12 @@ const data = [
 
 const buttons = ["Tất cả", "Chưa đọc", "Đã đọc"];
 
-const [active, setActive] = useState<number | null>(null);
-
-const handleClick = (index: number) => {
-  setActive(index);
-}
 
 const Notification = () => {
   return (
     <div className='w-full h-lvh bg-main1-1 flex flex-col'>
       <AdminTitle title='Thông báo' />
-      <div className='flex flex-row mx-8'>
-        {buttons.map((button, index) => (
-          <button key={index} className={`${active === index ? 'bg-main1-2' : 'bg-main1-1'} text-white font-sans font-semibold py-2 px-4 rounded-t-md`} onClick={() => handleClick(index)}>
-            {button}
-          </button>
-        ))}
-      </div>
+      
       <div className='w-full h-full '>
         <Table needHeader={needHeader} columns={columns} data={data} />
       </div>
