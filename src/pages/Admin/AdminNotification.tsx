fc81f97bc186a2e2cbee9b677768091c970dddb5
t6d <22022115@vnu.edu.vn>
2024-11-06 02:32:32 +0700

    add admin tab and add AdminAccounts

--- src/pages/Admin/AdminNotification.tsx
+++ src/pages/Admin/AdminNotification.tsx
@@ -1,5 +1,4 @@
-import { AdminTitle, Table } from '../../components'
-import React from 'react';
+import { AdminTab, AdminTitle, Table } from '../../components'
 
 const needHeader = false
 const columns = ["Status","Type", "Message", "Time"];
@@ -7,27 +6,17 @@ const data = [
   { Status: "1", Type: "Program", Message: "Yêu cầu tạo mới từ HackTheBox", Time: "1 ngày trước" },
   { Status: "1", Type: "Program", Message: "Yêu cầu tạo mới từ HackTheBox", Time: "2 ngày trước" },
   { Status: "1", Type: "Report", Message: "Yêu cầu tạo mới từ HackTheBox", Time: "3 ngày trước" },
-  ];
+];
 
 const buttons = ["Tất cả", "Chưa đọc", "Đã đọc"];
 
 const AdminNotification = () => {
-  const [active, setActive] = React.useState<number>(1);
-
-  const handleClick = (index: number) => {
-  setActive(index);
-  }
-
   return (
   <div className='w-full h-lvh bg-main1-1 flex flex-col'>
     <AdminTitle title='Thông báo' />
 
     <div className='ml-8'>
-    <div className='flex justify-start m'>
-      {buttons.map((button, index) => (
-      <button key={`tab_${index}`} className={`${active === index ? 'bg-main1-2' : 'bg-main1-1'} px-4 py-2 text-white rounded-t-md`} onClick={() => handleClick(index)}>{button}</button>
-      ))}
-    </div>
+    <AdminTab buttons={buttons}  />
     </div>
 
     <div className='w-full h-full '>
