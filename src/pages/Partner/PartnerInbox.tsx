9fed8d388e0d6ad1d22e416d0a4fe0ee03573774
caolephung <phungcl2k4@gmail.com>
2024-11-07 00:29:47 +0700

    update partner inbox

--- src/pages/Partner/PartnerInbox.tsx
+++ src/pages/Partner/PartnerInbox.tsx
@@ -6,40 +6,43 @@ interface CreateChatProps {
     reportName: string;
     userName: string;
     time: string;
+    onOpenChat: (userName: string) => void;
 }
 
-const CreateChat: React.FC<CreateChatProps> = ({ avt, reportName, userName, time }) => {
+const CreateChat: React.FC<CreateChatProps> = ({ avt, reportName, userName, time, onOpenChat }) => {
     return (
-        <div className="mt-[10px] bg-main1-2 w-full flex flex-row h-[60px] rounded-xl">
-            <div className="basis-[15%] w-full flex justify-center items-center">
-                <div id="userAvt" className="rounded-full w-[50%] h-[50%] flex justify-center items-center">
-                    <img src={avt} alt="" className="w-full"/>
+        <button onClick={() => onOpenChat(userName)}>
+            <div className="mt-[10px] bg-main1-2 w-full flex flex-row h-[60px] rounded-xl">
+                <div className="basis-[15%] w-full flex justify-center items-center">
+                    <div id="userAvt" className="rounded-full w-[50%] h-[50%] flex justify-center items-center">
+                        <img src={avt} alt="" className="w-full" />
+                    </div>
+                </div>
+                <div id="userName" className="basis-[45%] w-full h-[60px] flex items-center">
+                    <div>
+                        <div><span className="text-white"><b>{reportName}</b></span></div>
+                        <div><span className="text-main1-3">{userName}</span></div>
+                    </div>
+                </div>
+                <div className="basis-[30%] w-full flex items-center">
+                    <div>
+                        <div><span className="text-main1-3">Thời gian</span></div>
+                        <div><span className="text-main1-3">{time}</span></div>
+                    </div>
+                </div>
+                <div className="basis-[10%] w-full flex items-center">
+                    <div className="text-main2-1">B</div>
                 </div>
             </div>
-            <div id="userName" className="basis-[45%] w-full h-[60px] flex items-center">
-                <div>
-                    <div><span className="text-white"><b>{reportName}</b></span></div>
-                    <div><span className="text-main1-3">{userName}</span></div>
-                </div>
-            </div>
-            <div className="basis-[30%] w-full flex items-center">
-                <div>
-                    <div><span className="text-main1-3">Thời gian</span></div>
-                    <div><span className="text-main1-3">{time}</span></div>
-                </div>
-            </div>
-            <div className="basis-[10%] w-full flex items-center">
-                <button className="text-main2-1">B</button>
-            </div>
-        </div>
+        </button>
     );
 };
 
 const PartnerInbox: React.FC = () => {
     const navigate = useNavigate();
-
     const [searchTerm1, setSearchTerm1] = useState<string>('');
     const [searchTerm2, setSearchTerm2] = useState<string>('');
+    const [currentChat, setCurrentChat] = useState<string | null>(null); 
 
     const handleSearchChange1 = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchTerm1(e.target.value);
@@ -63,7 +66,10 @@ const PartnerInbox: React.FC = () => {
 
     const handleAddChat = () => {
         console.log('Thêm chat:', searchTerm2);
-        
+    };
+
+    const openChat = (userName: string) => {
+        setCurrentChat(userName); 
     };
 
     return (
@@ -83,12 +89,12 @@ const PartnerInbox: React.FC = () => {
                         className="border border-gray-300 rounded-full p-2 w-full"
                     />
                 </div>
-                <div className="basis-1/6"></div>
+                <div className="basis-1/6">Chuông</div>
             </div>
 
             <div className="flex flex-row w-full h-[90vh]">
                 <div className="flex flex-col basis-1/3 bg-main1-1 h-[100%] mr-[30px] p-[20px] rounded-2xl">
-                    <div className=" flex items-center w-full basis-[10%]"><span className="text-main1-3 text-2xl pl-[20px]">Đoạn chat</span></div>
+                    <div className="flex items-center w-full basis-[10%]"><span className="text-main1-3 text-2xl pl-[20px]">Đoạn chat</span></div>
                     
                     <div className="flex flex-row w-full items-center mb-[20px] basis-[10%]">
                         <div className="basis-2/3 mr-[10px]">
@@ -108,20 +114,52 @@ const PartnerInbox: React.FC = () => {
                     </div>
 
                     <div className="flex flex-col justify-start items-start w-full overflow-auto basis-[80%]"
-                    style={{
-                        maxHeight: 'auto',
-                        overflowY: 'auto', 
-                        scrollbarWidth: 'thin', 
-                        scrollbarColor: '#4a5568 transparent', 
-                      }}
+                        style={{
+                            maxHeight: 'auto',
+                            overflowY: 'auto', 
+                            scrollbarWidth: 'thin', 
+                            scrollbarColor: '#4a5568 transparent', 
+                        }}
                     >
-                        <CreateChat avt="/Logo_UET.png" reportName="RP1" userName="user1" time="10:10:10" />
-                        <CreateChat avt="/Logo_UET.png" reportName="RP1" userName="user1" time="10:10:10" />
-                        <CreateChat avt="/Logo_UET.png" reportName="RP1" userName="user1" time="10:10:10" />
+                        <CreateChat avt="/Logo_UET.png" reportName="RP1" userName="user1" time="10:10:10" onOpenChat={openChat} />
+                        <CreateChat avt="/Logo_UET.png" reportName="RP2" userName="user2" time="10:10:10" onOpenChat={openChat} />
                     </div>
                 </div>
-                <div className="flex flex-col basis-2/3 bg-main1-1">
-                    abc
+
+                <div className="basis-2/3 bg-main1-1 rounded-2xl">
+                    {currentChat ? (
+                        <div className="flex flex-col h-full">
+                            <div className="basis-[15%] flex flex-row bg-main1-2 border border-main2-1 rounded-t-xl">
+                                <div className="basis-[15%] flex justify-center items-center mr-[10px] w-full">
+                                    <img src="/Logo_UET.png" alt="" className="h-auto w-[50%] rounded-full"/>
+                                </div>
+                                <div className="basis-[75%] flex items-center w-full">
+                                    <span className="text-white text-xl">{currentChat}</span>
+                                </div>
+                                <div className="basis-[10%] flex items-center justify-center text-white w-full">
+                                    !
+                                </div>
+                            </div>
+                            
+                            <div className="basis-[70%]">
+                                message inbox
+                            </div>
+
+                            <div className="basis-[15%] flex flex-row border-t-[0.25px] border-main1-2">
+                                <div className="flex flex-row w-full items-center pl-[40px]">
+                                    <input 
+                                        type="text"
+                                        placeholder="Aa"
+                                        className="basis-[80%] border border-main2-1 rounded-xl p-2 w-full bg-main1-2"
+                                    />
+                                    <div className="basis-[10%] w-full">Gửi</div>
+                                    <div className="basis-[10%] w-full">Link</div>
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="text-gray-400"></div>
+                    )}
                 </div>
             </div>
         </div>
