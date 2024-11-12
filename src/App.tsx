7dbcd425cbf645c60ca63471980165f26ad64335
caolephung <phungcl2k4@gmail.com>
2024-11-12 19:18:26 +0700

    add partner overview

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css'
-import { Admin, AdminNotification, Home, Programs, LeaderBoard, SubmitReport, Profile, AdminPrograms, AdminAccounts, AdminReports, AdminSettings, Login, PartnerInbox, ResearchRegister, Reward } from './pages';
+import { Admin, AdminNotification, Home, Programs, LeaderBoard, SubmitReport, Profile, AdminPrograms, AdminAccounts, AdminReports, AdminSettings, Login, PartnerInbox, PartnerOverview, ResearchRegister, Reward } from './pages';
 import AdminLayout from './layouts/AdminLayout';
 import MainLayout from './layouts/MainLayout';
 import HomeLayout from './layouts/HomeLayout';
@@ -43,6 +43,7 @@ export default function App() {
 
         <Route element={<PartnerLayout />}>
           <Route path='/partner/inbox' element={<PartnerInbox />}/>
+          <Route path='/partner/overview' element={<PartnerOverview />}/>
         </Route>
 
       </Routes>
--- src/pages/Partner/index.ts
+++ src/pages/Partner/index.ts
@@ -1,1 +1,2 @@
 export {default as PartnerInbox} from './PartnerInbox'
+export {default as PartnerOverview} from './PartnerOverview'
