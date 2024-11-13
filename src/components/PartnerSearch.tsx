b7b5c8dd8ffdcf16d8dc7c21ccfb7d35b2b630de
caolephung <phungcl2k4@gmail.com>
2024-11-13 19:39:33 +0700

    update partner search component

--- src/components/PartnerSearch.tsx
+++ src/components/PartnerSearch.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaRegBell, FaRegHandPeace } from 'react-icons/fa6';
 
 interface PartnerSearchProps {
     searchTerm: string;
@@ -10,7 +11,10 @@ const PartnerSearch: React.FC<PartnerSearchProps> = ({ searchTerm, onSearchChang
     return (
         <div className="flex flex-row items-center justify-center w-full">
             <div className="basis-3/6">
-                <div><span className="text-main2-1 text-2xl">Hello Imposter</span></div>
+                <div className="flex flex-row">
+                    <span className="text-main2-1 text-2xl mr-[10px]">Hello Imposter </span>
+                    <span className="text-main2-1 text-2xl"><FaRegHandPeace /></span>
+                </div>
                 <div><span className="text-main1-3">Hãy để chúng tôi bảo vệ bạn</span></div>
             </div>
             <div className="basis-2/6">
@@ -23,7 +27,9 @@ const PartnerSearch: React.FC<PartnerSearchProps> = ({ searchTerm, onSearchChang
                     className="border border-gray-300 rounded-full p-2 w-full"
                 />
             </div>
-            <div className="basis-1/6 flex justify-center items-center">Chuông</div>
+            <div className="basis-1/6 flex justify-start items-center pl-[30px]">
+                <div className="bg-white rounded-lg p-[8px]"><FaRegBell className="fill-black size-7" /></div>
+            </div>
         </div>
     );
 };
