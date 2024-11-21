4d38ff843792305bc5401cdcb3595653480db745
caolephung <phungcl2k4@gmail.com>
2024-11-21 09:50:49 +0700

    add partner programs

---
+++ frontend/src/pages/Partner/PartnerPrograms.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PartnerPrograms: React.FC = () => {
+  return (
+    <div>PartnerPrograms</div>
+  )
+}
+
+export default PartnerPrograms
--- frontend/src/pages/Partner/index.ts
+++ frontend/src/pages/Partner/index.ts
@@ -1,3 +1,4 @@
 export {default as PartnerInbox} from './PartnerInbox'
 export {default as PartnerOverview} from './PartnerOverview'
 export {default as PaymentHistory} from './PaymentHistory'
+export {default as PartnerPrograms} from './PartnerPrograms'
