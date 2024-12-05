3e71b3b06dc27007b73869308f6d8e9ddd424af4
caolephung <phungcl2k4@gmail.com>
2024-12-05 18:16:41 +0700

    update user.ts

--- backend/src/entity/User.ts
+++ backend/src/entity/User.ts
@@ -15,6 +15,15 @@ export class User {
     @Column({ nullable: true }) // Có thể null
     email!: string;
 
+    @Column({ nullable: true }) // Có thể null
+    phoneNumber!: string;
+
+    @Column({ nullable: true }) // Có thể null
+    webSite!: string;
+
+    @Column({ nullable: true }) // Có thể null
+    biography!: string;
+
     @Column({ default: "active" }) // Giá trị mặc định
     status!: string;
 
