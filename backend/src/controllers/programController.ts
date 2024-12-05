8a3aed43d6b892c2a901385b4ddda360becf476f
caolephung <phungcl2k4@gmail.com>
2024-12-05 18:18:40 +0700

    fix program controller

--- backend/src/controllers/programController.ts
+++ backend/src/controllers/programController.ts
@@ -3,8 +3,14 @@ import { AppDataSource } from "../data-source";
 import { Program } from "../entity/Program";
 
 // Lấy danh sách chương trình
-export const program_get_list = async (req: Request, res: Response) => {
+export const program_get_list = async (req: Request, res: Response): Promise<void> => {
     const programRepository = AppDataSource.getRepository(Program);
     const programs = await programRepository.find();
     res.json(programs);
+    return;
+};
+
+// 
+export const program_approve = async (req: Request, res: Response): Promise<void> => {
+    
 };
