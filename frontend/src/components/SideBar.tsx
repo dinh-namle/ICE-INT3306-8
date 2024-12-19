e9d3da496734ea1b07256a3d822991edfd3118a9
caolephung <phungcl2k4@gmail.com>
2024-12-20 00:07:33 +0700

    fix sieber

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -32,7 +32,7 @@ const SideBar: React.FC = () => {
       <Link className={`py-4 ${location.pathname === "/inbox" ? "bg-main2-1" : "bg-main1-1"} w-full flex flex-col items-center`} to={"#"}>
         <IoMail className="fill-white size-6" />
       </Link>
-      <Link className={`py-4 ${location.pathname === "/profile" ? "bg-main2-1" : "bg-main1-1"} w-full flex flex-col items-center`} to={"/profile/1"}>
+      <Link className={`py-4 ${location.pathname.startsWith("/profile") ? "bg-main2-1" : "bg-main1-1"} w-full flex flex-col items-center`} to={"/profile/1"}>
         <FaUserCircle className="fill-white size-6" />
       </Link>
       <Link className={`py-4 ${location.pathname === "/payment" ? "bg-main2-1" : "bg-main1-1"} w-full flex flex-col items-center`} to={"/payout"}>
